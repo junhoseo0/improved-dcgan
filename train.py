78636fc0e05b50e0b3acce5ab5c8e848ff858179
TomatoPope0 <tomatopope0@gmail.com>
2020-08-08 12:59:20 +0900

    Load models based on arguments

--- train.py
+++ train.py
@@ -1,10 +1,17 @@
 import argparse
+from dcgan import Generator, Discriminator
 
 parser = argparse.ArgumentParser("Hyperparameters and Dataset Arguments")
-parser.add_argument("--dataset", "-d", type=str, default="mnist")
+parser.add_argument("--dataset", type=str, default="mnist")
+parser.add_argument("--num-noises", "-n", type=int, default=100)
+parser.add_argument("--colors", "-c", type=int, default=3)
+parser.add_argument("--depths", "-d", type=int, default=128)
 args = parser.parse_args()
 
 if args.dataset == "mnist":
-    from data_mnist import data, dataloader
+    from data_mnist import data, dataloader, IMAGE_SIZE
 else:
     raise Exception("Not a valid dataset")
+
+G = Generator(args.num_noises, args.colors, args.depths, IMAGE_SIZE)
+D = Discriminator(args.colors, args.depths, IMAGE_SIZE)
