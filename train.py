3f6b567f0796d4735347aa693acfd4b9e286fe37
TomatoPope0 <tomatopope0@gmail.com>
2020-08-08 13:23:04 +0900

    Move dataset initialization to training code; remove color channels from args

--- train.py
+++ train.py
@@ -1,11 +1,13 @@
 import argparse
 import torch
+import torchvision
+import torchvision.transforms as T
 from dcgan import Generator, Discriminator
 
 parser = argparse.ArgumentParser("Hyperparameters and Dataset Arguments")
 parser.add_argument("--dataset", "-ds", type=str, default="mnist")
+parser.add_argument("--batch-size", "-bs", type=int, default=64)
 parser.add_argument("--num-noises", "-n", type=int, default=100)
-parser.add_argument("--colors", "-c", type=int, default=3)
 parser.add_argument("--depths", "-d", type=int, default=128)
 parser.add_argument("--learning-rate", "-lr", type=float, default=0.0002)
 parser.add_argument("--beta-1", "-b1", type=float, default=0.5)
@@ -14,12 +16,29 @@ parser.add_argument("--epochs", "-e", type=int, default=10)
 args = parser.parse_args()
 
 if args.dataset == "mnist":
-    from data_mnist import data, dataloader, IMAGE_SIZE
+    IMAGE_SIZE=64
+    NUM_COLORS=1
+
+    data = torchvision.datasets.MNIST(
+        "../Datasets/MNIST_PyTorch",
+        train=True,
+        transform=T.Compose([
+            T.Resize(IMAGE_SIZE),
+            T.ToTensor(),
+            T.Normalize((0.5), (1))
+        ]),
+        download=True
+    )
+    dataloader = torch.utils.data.DataLoader(
+        data,
+        batch_size=args.batch_size,
+        shuffle=True
+    )
 else:
     raise Exception("Not a valid dataset")
 
-G = Generator(args.num_noises, args.colors, args.depths, IMAGE_SIZE)
-D = Discriminator(args.colors, args.depths, IMAGE_SIZE)
+G = Generator(args.num_noises, NUM_COLORS, args.depths, IMAGE_SIZE)
+D = Discriminator(NUM_COLORS, args.depths, IMAGE_SIZE)
 
 criterion = torch.nn.BCELoss()
 optimizer_g = torch.optim.Adam(
@@ -38,7 +57,7 @@ if __name__ == "__main__":
         for i, data in enumerate(dataloader):
             # Train D with genuine data
             genuine = data[0] # Drop label data
-            genuine = genuine.reshape(-1, args.colors, IMAGE_SIZE, IMAGE_SIZE)
+            genuine = genuine.reshape(-1, NUM_COLORS, IMAGE_SIZE, IMAGE_SIZE)
 
             optimizer_d.zero_grad()
 
