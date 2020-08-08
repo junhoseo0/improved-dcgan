b8eeab60e0c1d96a5a10b6313e65ea35299ed632
TomatoPope0 <tomatopope0@gmail.com>
2020-08-08 13:03:39 +0900

    Initialize optimizer based on args

--- train.py
+++ train.py
@@ -1,11 +1,15 @@
 import argparse
+import torch
 from dcgan import Generator, Discriminator
 
 parser = argparse.ArgumentParser("Hyperparameters and Dataset Arguments")
-parser.add_argument("--dataset", type=str, default="mnist")
+parser.add_argument("--dataset", "-ds", type=str, default="mnist")
 parser.add_argument("--num-noises", "-n", type=int, default=100)
 parser.add_argument("--colors", "-c", type=int, default=3)
 parser.add_argument("--depths", "-d", type=int, default=128)
+parser.add_argument("--learning-rate", "-lr", type=float, default=0.0002)
+parser.add_argument("--beta-1", "-b1", type=float, default=0.5)
+parser.add_argument("--beta-2", "-b2", type=float, default=0.99)
 args = parser.parse_args()
 
 if args.dataset == "mnist":
@@ -15,3 +19,15 @@ else:
 
 G = Generator(args.num_noises, args.colors, args.depths, IMAGE_SIZE)
 D = Discriminator(args.colors, args.depths, IMAGE_SIZE)
+
+criterion = torch.nn.CrossEntropyLoss()
+optimizer_G = torch.optim.Adam(
+    G.parameters(),
+    lr=args.learning_rate,
+    betas=[args.beta_1, args.beta_2]
+)
+optimizer_D = torch.optim.Adam(
+    D.parameters(),
+    lr=args.learning_rate,
+    betas=[args.beta_1, args.beta_2]
+)
